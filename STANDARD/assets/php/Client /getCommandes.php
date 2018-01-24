c08f3114f4cc689adb5eb3c52be33040314c1a64
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-01-24 14:05:21 +0000

    Update getCommandes.php

--- STANDARD/assets/php/Client /getCommandes.php
+++ STANDARD/assets/php/Client /getCommandes.php
@@ -10,7 +10,6 @@
 header('Access-Control-Allow-Origin: *');
 $conn = mysqli_connect("phpmyadmin.ctjo7qihl13z.us-east-2.rds.amazonaws.com","phpMyAdmin","phpMyAdmin","pressing");
 
-$conn = mysqli_connect("localhost", "root", "", "Pressing");
 $info = json_decode(file_get_contents("php://input"));
 $output = array();
 $IdUser   = mysqli_real_escape_string($conn, $info->IdUser);
