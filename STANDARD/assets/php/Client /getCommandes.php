6f5ae7331fc597197be0d353e0a35eead68d09b8
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-01-24 14:10:06 +0000

    Update getCommandes.php

--- STANDARD/assets/php/Client /getCommandes.php
+++ STANDARD/assets/php/Client /getCommandes.php
@@ -7,30 +7,34 @@
 //   AND CLIENT.Password = 'client1' 
 //   AND CLIENT.TELEPHONE_CLIENT = '622529293' 
 //   AND COMMANDE.STATUS != '%livre%'"
-header('Access-Control-Allow-Origin: *');
-$conn = mysqli_connect("phpmyadmin.ctjo7qihl13z.us-east-2.rds.amazonaws.com","phpMyAdmin","phpMyAdmin","pressing");
 
-$info = json_decode(file_get_contents("php://input"));
-$output = array();
-$IdUser   = mysqli_real_escape_string($conn, $info->IdUser);
-
-$output = [];
-// $output = array();
-// $query  = "SELECT * FROM `COMMANDE`";
-$query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.DD_COMMANDE , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE,LOCALISATION.Adresse_Complete_Collect
-FROM COMMANDE , LOCALISATION
-WHERE COMMANDE.ID_CLIENT = '$IdUser'
-AND COMMANDE.ID_LOCALISATION = LOCALISATION.ID_LOCALISATION
-AND COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT')  ";
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) > 0) {
-while ($row = mysqli_fetch_array($result)) {
-$output[] = $row;
-}
-// echo json_encode($output);
-}
-
-echo json_encode($output);
-?> 
 
  
+
+<?php 
+header('Access-Control-Allow-Origin: *');
+// header('Content-Type:application/json');
+$response= [];
+if(count($_POST)>0) {
+$conn = mysqli_connect("phpmyadmin.ctjo7qihl13z.us-east-2.rds.amazonaws.com","phpMyAdmin","phpMyAdmin","pressing");
+$result = mysqli_query($conn,"SELECT COMMANDE.ID_COMMANDE , COMMANDE.DD_COMMANDE , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE,LOCALISATION.Adresse_Complete_Collect
+FROM COMMANDE , LOCALISATION
+WHERE COMMANDE.ID_CLIENT = ".$_POST["Iduser"]."
+AND COMMANDE.ID_LOCALISATION = LOCALISATION.ID_LOCALISATION
+AND COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT') ;
+    if(mysqli_num_rows($result)>0){
+    while($row = $result->fetch_assoc()){
+         // $response['status']= "loggedin";   
+        $response[] = $row;
+        // $response['status']=$row['STATUS'];	
+        //  $_SESSION['']= $row['TELEPHONE_CLIENT'];
+    //      echo "connected";
+    // echo $row['ID_CLIENT'];
+   
+    }
+    
+    }
+    }
+    
+echo json_encode($response);
+?>
