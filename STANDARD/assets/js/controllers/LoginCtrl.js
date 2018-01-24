c1153e803232bd43853d2e9144044ee7bb767101
hamzaelha01 <35404886+hamzaelha01@users.noreply.github.com>
2018-01-24 00:07:01 +0000

    Update LoginCtrl.js

--- STANDARD/assets/js/controllers/LoginCtrl.js
+++ STANDARD/assets/js/controllers/LoginCtrl.js
@@ -10,7 +10,7 @@ app.controller('LoginCtrl', ["$scope", "$window", "$http", "user", function($sco
         // alert(number);
         // alert(pass);
         $http({
-            url: 'http://http://18.221.242.75/STANDARD/assets/php/Login/login.php',
+            url: 'http://18.221.242.75:3000/PADv1/STANDARD/assets/php/Login/login.php',
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
