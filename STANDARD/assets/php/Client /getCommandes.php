d56c97957b42827183630082c2b9a6d9e972ee97
PAD <mac@macs-MacBook-Pro.local>
2018-01-26 18:16:34 +0000

    v10

--- STANDARD/assets/php/Client /getCommandes.php
+++ STANDARD/assets/php/Client /getCommandes.php
@@ -6,32 +6,95 @@
 //   AND PANIER.ID_CLIENT = CLIENT.ID_CLIENT 
 //   AND CLIENT.Password = 'client1' 
 //   AND CLIENT.TELEPHONE_CLIENT = '622529293' 
+//   AND COMMANDE.STATUS != '%livre%'" 
+//  $query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.NBR_ARTICLES , COMMANDE.DD_COMMANDE , COMMANDE.DF_COMMANDE, PANIER.QUANITE , PANIER.MONTANT 
+// FROM PANIER , COMMANDE , CLIENT
+//  WHERE PANIER.ID_COMMANDE = COMMANDE.ID_COMMANDE
+//   AND PANIER.ID_CLIENT = CLIENT.ID_CLIENT 
+//   AND CLIENT.Password = 'client1' 
+//   AND CLIENT.TELEPHONE_CLIENT = '622529293' 
 //   AND COMMANDE.STATUS != '%livre%'"
-header('Access-Control-Allow-Origin: *');
-$conn = mysqli_connect("phpmyadmin.ctjo7qihl13z.us-east-2.rds.amazonaws.com","phpMyAdmin","phpMyAdmin","pressing");
 
-$conn = mysqli_connect("localhost", "root", "", "Pressing");
-$info = json_decode(file_get_contents("php://input"));
-$output = array();
-$IdUser   = mysqli_real_escape_string($conn, $info->IdUser);
+//$conn = mysqli_connect("localhost", "root", "", "Pressing");
+// $info = json_decode(file_get_contents("php://input"));
+// $output = array();
+
+// $IdUser   = mysqli_real_escape_string($conn, $info->IdUser);
 
 
 // $output = array();
 // $query  = "SELECT * FROM `COMMANDE`";
-$query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.DD_COMMANDE , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE,LOCALISATION.Adresse_Complete_Collect
-FROM COMMANDE , LOCALISATION
-WHERE COMMANDE.ID_CLIENT = 1
-AND COMMANDE.ID_LOCALISATION = LOCALISATION.ID_LOCALISATION
-AND COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT')  ";
-$result = mysqli_query($conn, $query);
-if (mysqli_num_rows($result) > 0) {
-while ($row = mysqli_fetch_array($result)) {
-$output[] = $row;
-}
+// $query  ="";
+// $result = mysqli_query($conn, $query);
+// if (mysqli_num_rows($result) > 0) {
+// while ($row = mysqli_fetch_array($result)) {
+// $output[] = $row;
+// }
 // echo json_encode($output);
-}
+// }
 
-echo json_encode($output);
-?> 
+// echo json_encode($output);
+
 
  
+
+
+
+header('Access-Control-Allow-Origin: *');
+// header('Content-Type:application/json');
+
+
+
+
+$response= [];
+if(count($_POST)>0) {
+	$conn = mysqli_connect("phpmyadmin.ctjo7qihl13z.us-east-2.rds.amazonaws.com","phpMyAdmin","phpMyAdmin","pressing");
+	$result = mysqli_query($conn,"SELECT COMMANDE.ID_COMMANDE , COMMANDE.DD_COMMANDE , COMMANDE.STATUS ,COMMANDE.NBR_ARTICLES , COMMANDE.DF_COMMANDE,LOCALISATION.Adresse_Complete_Collect FROM COMMANDE , LOCALISATION WHERE COMMANDE.ID_LOCALISATION = LOCALISATION.ID_LOCALISATION AND COMMANDE.STATUS IN ('EN ATTENTE','CONFIRME','TO COLLECT') AND COMMANDE.ID_CLIENT = ".$_POST["Iduser"]."  ");
+    if(mysqli_num_rows($result)>0){
+    while($row = $result->fetch_assoc()){
+         // $response['status']= "loggedin";   
+        $response[] = $row;
+        // $response['status']=$row['STATUS'];	
+
+        //  $_SESSION['']= $row['TELEPHONE_CLIENT'];
+    //      echo "connected";
+    // echo $row['ID_CLIENT'];
+   
+    }
+    
+    }
+    }
+    
+echo json_encode($response);
+
+
+
+
+
+
+// $conn = mysqli_connect("localhost", "root", "", "Pressing");
+// $info = json_decode(file_get_contents("php://input"));
+// $output = array();
+// $IdUser   = mysqli_real_escape_string($conn, $info->IdUser);
+
+
+// // $output = array();
+// // $query  = "SELECT * FROM `COMMANDE`";
+// $query  ="SELECT COMMANDE.ID_COMMANDE , COMMANDE.DD_COMMANDE , COMMANDE.STATUS  FROM COMMANDE 
+// WHERE COMMANDE.ID_CLIENT = '$IdUser'
+// AND  COMMANDE.STATUS like '%EN ATTENTE%'";
+// $result = mysqli_query($conn, $query);
+// if (mysqli_num_rows($result) > 0) {
+// while ($row = mysqli_fetch_array($result)) {
+// $output[] = $row;
+// }
+// // echo json_encode($output);
+// }
+
+// echo json_encode($output);
+?> 
+
+
+
+
+
